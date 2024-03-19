214be2a8e0babf8dc61e88f3a4c9fdfaeabaeb12
pedro <p.damianoriega@gmail.com>
2024-03-18 22:18:00 -0500

    encriptador HTML-CSS-JS

--- js/script.js
+++ js/script.js
@@ -1,41 +1,105 @@
 
-const textarea = document.getElementById("textarea");
-const btnEncriptar = document.getElementById("btnEncriptar");
-const btnDesencriptar = document.getElementById("btnDesencriptar");
-const btnCopiar = document.getElementById("btnCopiar");
-const mensajeFinal = document.getElementById("mensajeFinal");
-const munheco = document.getElementById("munheco");
-const infoMensaje = document.getElementById("infoMensaje");
-const container_right = document.getElementById("container_right")
 
-let reemplazar = [
-  ["e", "enter"],
-  ["o", "ober"],
-  ["i", "imes"],
-  ["a", "ai"],
-  ["u", "ufat"],
-]
+var btnEncriptar = document.querySelector(".btn-encriptar");
+var btnDesencriptar = document.querySelector(".btn-desencriptar");
+var resultado = document.querySelector(".mensaje");
+var muneco = document.querySelector(".muneco");
+var info = document.querySelector(".info-mensaje");
 
-btnEncriptar.addEventListener("click", () => { 
-  const texto = textarea.value.toLowerCase()
-  function encriptar(newText){
-    for(let i=0; i< reemplazar.length; i++){
-      if(newText.includes(reemplazar[i][0])){
-        newText = newText.replaceAll(reemplazar[i][0], reemplazar[i][1]);
-      };
-    };
-    return newText
+btnEncriptar.onclick = encriptar;
+btnDesencriptar.onclick = desencriptar;
+
+
+function encriptar(){
+  ocultarAdelante();
+  var mensaje = recuperarTexto()
+  resultado.textContent = encriptarTexto(mensaje);
+}
+
+
+function desencriptar(){
+  ocultarAdelante();
+  var mensaje = recuperarTexto()
+  resultado.textContent = desencriptarTexto(mensaje);
+}
+
+function recuperarTexto(){
+  var mensaje = document.querySelector(".text-area")
+  return mensaje.value
+}
+
+function ocultarAdelante(){
+  muneco.classList.add("ocultar");
+  info.classList.add("ocultar");
+}
+
+function encriptarTexto(mensaje){
+  var texto = mensaje;
+  var textoFinal = "";
+
+  for(var i = 0; i < texto.length; i++){
+      if(texto[i] == "a"){
+          textoFinal = textoFinal + "ai"
+      }
+      else if(texto[i] == "e"){
+          textoFinal = textoFinal + "enter"
+      }
+      else if(texto[i] == "i"){
+          textoFinal = textoFinal + "imes"
+      }
+      else if(texto[i] == "o"){
+          textoFinal = textoFinal + "ober"
+      }
+      else if(texto[i] == "u"){
+          textoFinal = textoFinal + "ufat"
+      }
+      else{
+          textoFinal = textoFinal + texto[i]
+      }
   }
-  const textoEncriptado = encriptar(texto);
-  mensajeFinal.innerHTML = textoEncriptado;
+  return textoFinal;
 
-  // oculta la imagen y el mensaje de informacion del contenedor derecho
-  munheco.style.display = "none";
-  infoMensaje.style.display = "none";
+}
 
-  // muestra el boton de copiar texto
-  btnCopiar.style.display = "block";
+function desencriptarTexto(mensaje){
+  var texto = mensaje;
+  var textoFinal = "";
 
-  container_right.classList.add("ajustar");
-  mensajeFinal.classList.add("ajustar");
+  for(var i = 0; i < texto.length; i++){
+      if(texto[i] == "a"){
+          textoFinal = textoFinal + "a"
+          i = i+1;
+      }
+      else if(texto[i] == "e"){
+          textoFinal = textoFinal + "e"
+          i = i+4;
+      }
+      else if(texto[i] == "i"){
+          textoFinal = textoFinal + "i"
+          i = i+3;
+      }
+      else if(texto[i] == "o"){
+          textoFinal = textoFinal + "o"
+          i = i+3;
+      }
+      
+      else if(texto[i] == "u"){
+          textoFinal = textoFinal + "u"
+          i = i+3;
+      }
+      else{
+          textoFinal = textoFinal + texto[i];
+      }
+  }
+
+  return textoFinal;
+
+}
+
+
+const btnCopiar = document.querySelector(".btn-copiar"); 
+    btnCopiar.addEventListener("click", copiar = () => {
+    var contenido = document.querySelector(".mensaje").textContent;
+    navigator.clipboard.writeText(contenido);
+    console.log("hola"); 
 });
